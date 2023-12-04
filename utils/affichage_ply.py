5580559e2680f030831259fa88a7099a7f1b7a0a
ThibaudPiccinali <100284370+ThibaudPiccinali@users.noreply.github.com>
2023-12-04 16:40:36 +0100

    PEP8

--- utils/affichage_ply.py
+++ utils/affichage_ply.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial import Delaunay
 
+
 def read_ply(file_path: str) -> np.ndarray:
     """
     Read a PLY file and extract vertices.
@@ -17,10 +18,12 @@ def read_ply(file_path: str) -> np.ndarray:
         lines = file.readlines()
 
     data_start = lines.index('end_header\n') + 1
-    data = [list(map(float, line.strip().split())) for line in lines[data_start:]]
+    data = [list(map(float, line.strip().split()))
+            for line in lines[data_start:]]
     vertices = np.array(data)
 
     return vertices
+
 
 def plot_ply(vertices: np.ndarray) -> None:
     """
@@ -35,7 +38,8 @@ def plot_ply(vertices: np.ndarray) -> None:
     """
     # Extract color information if available
     if vertices.shape[1] == 6:
-        colors = vertices[:, 3:6] / 255.0  # Normalize colors to the range [0, 1]
+        # Normalize colors to the range [0, 1]
+        colors = vertices[:, 3:6] / 255.0
     else:
         colors = 'b'  # Use blue as default color if no color information is present
 
@@ -46,10 +50,11 @@ def plot_ply(vertices: np.ndarray) -> None:
     ax = fig.add_subplot(111, projection='3d')
 
     # Plot the vertices with colors
-    ax.scatter(vertices[:, 0], vertices[:, 1], vertices[:, 2], c=colors, marker='o')
+    ax.scatter(vertices[:, 0], vertices[:, 1],
+               vertices[:, 2], c=colors, marker='o')
 
     # Plot the triangles
-    ax.plot_trisurf(vertices[:, 0], vertices[:, 1], vertices[:, 2], triangles=triangles.simplices, color='gray', alpha=0.5)
+    ax.plot_trisurf(vertices[:, 0], vertices[:, 1], vertices[:, 2],
+                    triangles=triangles.simplices, color='gray', alpha=0.5)
 
     plt.show()
-
